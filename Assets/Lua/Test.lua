6311022003915c1b04310d3abfa81c478e4d7113
youlanhai <you_lan_hai@foxmail.com>
2016-04-04 23:01:52 +0800

    add comment

--- Assets/Lua/Test.lua
+++ Assets/Lua/Test.lua
@@ -1,7 +1,9 @@
 
+--构造Test类
 local Test = {}
 Test.__index = Test
 
+--给Test类实例化一个对象
 function Test.New(cls)
 	print("Test:New", cls)
 
@@ -10,6 +12,7 @@ function Test.New(cls)
 	return self
 end
 
+--Awake方法
 function Test:Awake()
 	print("Test:Awake", self)
 end
